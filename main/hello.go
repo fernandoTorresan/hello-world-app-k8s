75a2d10f6d6f83d072af04052b3712c91dde2c4d
Fernando Torresan <fernandotdo61@gmail.com>
2020-02-02 16:25:20 -0300

    Adding application, Dockerfile and k8s deployment specification

---
+++ main/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api/hello", Hello)
+	http.ListenAndServe("0.0.0.0:8080", handler)
+	// Listen to all request to port 8080.
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Hello world`)
+}
